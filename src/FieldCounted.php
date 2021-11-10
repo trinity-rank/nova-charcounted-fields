3fb038f5785ffac0c3f06fc00e2f2b3df3c29afa
nebojsaTrinity <nebojsa@trinityrank.com>
2021-11-10 16:30:31 +0100

    first commit

---
+++ src/FieldCounted.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace TrinityRank\CharcountedFields;
+
+use Laravel\Nova\Fields\Field;
+
+class FieldCounted extends Field
+{
+
+    public function maxChars(int $characters)
+    {
+        return $this->withMeta(['maxChars' => $characters]);
+    }
+
+    public function warningAt(int $characters)
+    {
+        return $this->withMeta(['warningAt' => $characters]);
+    }
+
+}
